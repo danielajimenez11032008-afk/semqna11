577328bd906248cbfb86fa4eed11c670e4e05bfd
danielajimenez11032008-afk <danielajimenez11032008@gmail.com>
2026-03-03 16:27:22 -0500

    Add files via upload

---
+++ # Crear un arreglo con los números.py
@@ -0,0 +1,11 @@
+# Crear un arreglo con los números
+numeros = [10, 20, 30, 40, 50]
+
+# Mostrar el número en la posición 1
+print("Número en posición 1:", numeros[1])
+
+# Recorrer el arreglo
+print("\nRecorriendo el arreglo:")
+for numero in numeros:
+    print(numero)
+    
